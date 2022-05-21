3c54e3c0a3cb35020f775d8c6a0acaad0200656a
Dano4git <104991958+Dano4git@users.noreply.github.com>
2022-05-21 17:24:27 +0700

    Update 19May-2.js

--- js/19May-2.js
+++ js/19May-2.js
@@ -1,25 +1,64 @@
 function init () {
-    const nope= document.getElementById("nope");
-    
+    const ipMail= document.getElementById("ip-mail");
+    const ipPass= document.getElementById("ip-pass");
     const btnLogIn = document.getElementById("btn-login");
     const btnSignUp = document.getElementById("btn-signup");
+    const styleDiv = document.getElementsByClassName("login");
     
-    const paragraph = document.getElementById("status");
-
+    let loginBackground = btnLogIn.style.background;
+    let signupBackground = btnSignUp.style.background;
+    let messages = document.createElement("p");
+    messages.style.color = "red";
+    messages.style.fontSize = "12px";
     // Code ở đây
-    //B1
-    button.addEventListener("click", () => {
-    	paragraph.innerText = "Tôi giàu sau khi học lập trình tại mindX";
+    btnLogIn.addEventListener("click", (event) => {
+    	event.preventDefault();
+    })
+    btnLogIn.addEventListener("click", () => {  // submit can be use
+    	if (ipMail.value === "") {
+      	 messages.innerHTML = "Email must not be empty" ;
+         
+      	 insertAfter(ipPass, messages);
+         /* styleDiv[0].style.height = "290px"; */
+      } else if (!(/\@gmail\.com/i.test(ipMail.value))) {
+      	messages.innerHTML = "Email must be an email (@gmail.com)" ;
+        insertAfter(ipPass, messages);
+      	/* alert("Email must be an email (@gmail.com)"); */
+      } else if (ipPass.value.length <= 7) {      
+    		messages.innerHTML = "Password must be included more than 8 characters";
+        insertAfter(ipPass, messages);
+      } else {
+      	if (ipMail.value === "doanhoang@gmail.com" && ipPass.value === "123456789") {
+        	messages.innerHTML = "OK!";
+          insertAfter(ipPass, messages);
+        } else {
+        	messages.innerHTML = "Wrong email or password!";
+          insertAfter(ipPass, messages);
+        }
+      }
+ /*      alert(ipMail.value === "doanhoang@gmail.com");
+      alert(ipPass.value === "123456789"); */
     });
-    //B2
+   
     btnLogIn.addEventListener("mouseover", () => {
-  	  btnLogIn.style.background = "red";
+  	  btnLogIn.style.background = "#1338BE";
     });
-    //B3
-    nope.addEventListener("mouseout", () => {
-  	  nope.style.color = "";
-  	  nope.style.background = "";
+    
+    btnLogIn.addEventListener("mouseout", () => {
+  	  btnLogIn.style.background = loginBackground;
     });
+    
+    btnSignUp.addEventListener("mouseover", () => {
+  	btnSignUp.style.background = "mediumseagreen";
+    });
+  
+    btnSignUp.addEventListener("mouseout", () => {
+  	  btnSignUp.style.background = signupBackground;
+    });
+    
+    function insertAfter(referenceNode, newNode) {
+  		referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+}
 }
 
 
